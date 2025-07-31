37b14ec94d7ccd543b531cd4c03713d26096310d
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-01 01:58:09 +0530

    Time: 119 ms (5.29%) | Memory: 13.2 MB (88.52%) - LeetSync

--- Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,27 +1,29 @@
 #dsajourney-MV
 class Solution(object):
     def removeDuplicates(self, nums):
-        # k = 1
-        # i = 1
-        # n = len(nums)
-        # while i < n:
-        #     if nums[i] == nums[i-1]:
-        #         value = nums[i]
-        #         nums.pop(i)
-        #         nums.append(value)
-        #         n -= 1
-        #     else:
-        #         i += 1
-        #         k += 1
-        # return k
-        if not nums:
-            return 0
-        
-        k = 1  
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:  
-                nums[k] = nums[i]
+        k = 1
+        i = 1
+        n = len(nums)
+        while i < n:
+            if nums[i] == nums[i-1]:
+                value = nums[i]
+                nums.pop(i)
+                nums.append(value)
+                n -= 1
+            else:
+                i += 1
                 k += 1
-
         return k
+
+        #Better approach
+        # if not nums:
+        #     return 0
+        
+        # k = 1  
+
+        # for i in range(1, len(nums)):
+        #     if nums[i] != nums[i - 1]:  
+        #         nums[k] = nums[i]
+        #         k += 1
+
+        # return k
