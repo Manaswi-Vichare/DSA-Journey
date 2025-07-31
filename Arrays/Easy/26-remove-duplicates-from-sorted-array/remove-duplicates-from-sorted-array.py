42758c78949d9eb1a8305f58d90a00c4a0dc7aeb
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-01 02:23:29 +0530

    Time: 3 ms (61.83%) | Memory: 14 MB (37.47%) - LeetSync

--- Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,29 +1,27 @@
 #dsajourney-MV
+#Better Approach:
 class Solution(object):
     def removeDuplicates(self, nums):
-        k = 1
-        i = 1
-        n = len(nums)
-        while i < n:
-            if nums[i] == nums[i-1]:
-                value = nums[i]
-                nums.pop(i)
-                nums.append(value)
-                n -= 1
-            else:
-                i += 1
-                k += 1
-        return k
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l += 1
+        return l
 
-        #Better approach
-        # if not nums:
-        #     return 0
-        
-        # k = 1  
-
-        # for i in range(1, len(nums)):
-        #     if nums[i] != nums[i - 1]:  
-        #         nums[k] = nums[i]
-        #         k += 1
-
-        # return k
+#First Approach:
+# class Solution(object):
+#     def removeDuplicates(self, nums):
+#         k = 1
+#         i = 1
+#         n = len(nums)
+#         while i < n:
+#             if nums[i] == nums[i-1]:
+#                 value = nums[i]
+#                 nums.pop(i)
+#                 nums.append(value)
+#                 n -= 1
+#             else:
+#                 i += 1
+#                 k += 1
+#         return k
