aadbc6ee4ab8b1619d8d180eb0143d3977a06981
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-03 02:48:41 +0530

    Time: 8 ms (92.56%) | Memory: 15.5 MB (6.26%) - LeetSync

---
+++ Strings/Easy/125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,23 @@
+#dsajourney-MV
+import math
+import re
+class Solution(object):
+    def isPalindrome(self, s):
+        i, j = 0, 0
+        front, back = 0, 0
+
+        s = s.lower()
+        s = s.replace(" ", "")
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        if len(s) == 1 or len(s) == 0:
+            return True          
+
+        half = int(math.floor(len(s)/2))
+        while i < half and j < half:
+            for i in range(0, half):
+                for j in range(len(s) - 1, half - 1, -1):
+                    if s[i] != s[j]:
+                        return False
+                    else:
+                        i += 1
+                return True
