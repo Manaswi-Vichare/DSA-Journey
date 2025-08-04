ae593152264051b8574a23a5489d1d6446076aff
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-04 21:29:26 +0530

    Time: 134 ms (24.96%) | Memory: 13 MB (68.80%) - LeetSync

--- Strings/Easy/125-valid-palindrome/valid-palindrome.py
+++ Strings/Easy/125-valid-palindrome/valid-palindrome.py
@@ -1,23 +1,34 @@
 #dsajourney-MV
-import math
-import re
+#Using in-built function
 class Solution(object):
     def isPalindrome(self, s):
-        i, j = 0, 0
-        front, back = 0, 0
+        new_s = ""
+        for c in s:
+            if c.isalnum():
+                new_s += c.lower()
+        return new_s == new_s[::-1]
 
-        s = s.lower()
-        s = s.replace(" ", "")
-        s = re.sub(r'[^a-zA-Z0-9]', '', s)
-        if len(s) == 1 or len(s) == 0:
-            return True          
 
-        half = int(math.floor(len(s)/2))
-        while i < half and j < half:
-            for i in range(0, half):
-                for j in range(len(s) - 1, half - 1, -1):
-                    if s[i] != s[j]:
-                        return False
-                    else:
-                        i += 1
-                return True
+
+#First approach
+# import math
+# import re
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         i, j = 0, 0
+#         front, back = 0, 0
+#         s = s.lower()
+#         s = s.replace(" ", "")
+#         s = re.sub(r'[^a-zA-Z0-9]', '', s)
+#         if len(s) == 1 or len(s) == 0:
+#             return True          
+
+#         half = int(math.floor(len(s)/2))
+#         while i < half and j < half:
+#             for i in range(0, half):
+#                 for j in range(len(s) - 1, half - 1, -1):
+#                     if s[i] != s[j]:
+#                         return False
+#                     else:
+#                         i += 1
+#                 return True
