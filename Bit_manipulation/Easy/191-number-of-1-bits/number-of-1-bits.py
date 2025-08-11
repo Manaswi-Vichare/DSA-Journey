026cc099b0aaf6c7f943cd4fb75ff4cd3286b0c6
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-12 02:59:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (79.10%) - LeetSync

--- Bit_manipulation/Easy/191-number-of-1-bits/number-of-1-bits.py
+++ Bit_manipulation/Easy/191-number-of-1-bits/number-of-1-bits.py
@@ -1,10 +1,20 @@
+#No in-built functions
 class Solution(object):
     def hammingWeight(self, n):
-        b = bin(n)[2:]
-        print(b)
-        print(len(b))
-        count = 0
-        for i in range(0, len(b)):
-            if b[i] == '1':
-                count += 1
-        return count
+        res = 0
+        for i in range(32):
+            if (n >> i) & 1:
+                res += 1
+        return res
+
+#First Approach
+# class Solution(object):
+#     def hammingWeight(self, n):
+#         b = bin(n)[2:]
+#         print(b)
+#         print(len(b))
+#         count = 0
+#         for i in range(0, len(b)):
+#             if b[i] == '1':
+#                 count += 1
+#         return count
