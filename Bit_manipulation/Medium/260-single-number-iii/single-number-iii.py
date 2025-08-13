871f966c03746ce740d8a6781981fb40047e7612
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-14 02:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (96.81%) - LeetSync

--- Bit_manipulation/Medium/260-single-number-iii/single-number-iii.py
+++ Bit_manipulation/Medium/260-single-number-iii/single-number-iii.py
@@ -1,14 +1,46 @@
 #dsajourney-MV
+#BIT MANIPULATION - TC: O(N), SC: O(1)
 class Solution(object):
     def singleNumber(self, nums):
+        xor = 0
+        for n in nums:
+            xor ^= n
+        
+        set_bit = xor & -xor
+    
         i, j = 0, 0
-        result = []
-        for i in range(len(nums)):
-            duplicate = False
-            for j in range(len(nums)):
-                if nums[i] == nums[j] and i != j:
-                    duplicate = True
-                    break
-            if not duplicate:
-                result.append(nums[i])
-        return result
+        for num in nums:
+            if num & set_bit:
+                i ^= num
+            else:
+                j ^= num
+        return [i, j]
+
+
+#FREQUENCY MAP - TC, SC: O(N)
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         freq = defaultdict(int)
+#         for n in nums:
+#             freq[n] += 1
+#             print(n, freq[n])
+#         res = []
+#         for num, f in freq.items():
+#             if f == 1:
+#                 res.append(num)
+#         return res
+
+#BRUTE-FORCE - TC: O(N^2), SC: O(1)
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         i, j = 0, 0
+#         result = []
+#         for i in range(len(nums)):
+#             duplicate = False
+#             for j in range(len(nums)):
+#                 if nums[i] == nums[j] and i != j:
+#                     duplicate = True
+#                     break
+#             if not duplicate:
+#                 result.append(nums[i])
+#         return result
