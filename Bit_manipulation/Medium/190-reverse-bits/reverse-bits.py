594fa37c70e755da1ccf525f0f4dce801e82b564
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-14 00:41:43 +0530

    Delete Bit_manipulation/Medium/190-reverse-bits directory

--- Bit_manipulation/Medium/190-reverse-bits/reverse-bits.py
+++
@@ -1,8 +0,0 @@
-#sqljourney-MV
-class Solution(object):
-    def reverseBits(self, n):
-        ans = 0
-        for i in range(32):
-            ans = (ans << 1) + (n & 1)
-            n = n >> 1
-        return ans
