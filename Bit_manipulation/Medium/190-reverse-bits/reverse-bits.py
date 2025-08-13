2fb19883d044885cf220feccc5b39a3f403298b7
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-14 00:41:05 +0530

    Time: 11 ms (92.77%) | Memory: 12.5 MB (8.18%) - LeetSync

---
+++ Bit_manipulation/Medium/190-reverse-bits/reverse-bits.py
@@ -0,0 +1,8 @@
+#sqljourney-MV
+class Solution(object):
+    def reverseBits(self, n):
+        ans = 0
+        for i in range(32):
+            ans = (ans << 1) + (n & 1)
+            n = n >> 1
+        return ans
