5fe0d9c535ff59ae564bad69a9b24d25fbb5a7bc
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-15 03:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (88.64%) - LeetSync

--- Strings/Easy/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ Strings/Easy/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -1,19 +1,29 @@
 #dsajourney-MV
+# class Solution(object):
+#     def largestGoodInteger(self, num):
+#         if len(num) < 3:
+#             return ""
+
+#         count = 0
+#         res = ''
+#         for i in range(1, len(num)):
+#             if num[i] == num[i-1]:
+#                 count += 1
+#             else:
+#                 count = 0
+
+#             if (count == 2):
+#                 triple = num[i-2 : i+1]
+#                 if res == "" or triple > res:
+#                     res = triple
+#         return res
+
+
 class Solution(object):
     def largestGoodInteger(self, num):
-        if len(num) < 3:
-            return ""
-
-        count = 0
-        res = ''
-        for i in range(1, len(num)):
-            if num[i] == num[i-1]:
-                count += 1
-            else:
-                count = 0
-
-            if (count == 2):
-                triple = num[i-2 : i+1]
-                if res == "" or triple > res:
-                    res = triple
-        return res
+        new="9876543210"
+        for i in new:
+            if i*3 in num:
+                return i*3
+            
+        return ""
