8b9d11db7bd881e3415398a58609b0940b5a2b7e
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-18 02:37:40 +0530

    Time: 19 ms (28.47%) | Memory: 13.4 MB (29.64%) - LeetSync

--- Arrays/Easy/268-missing-number/missing-number.py
+++ Arrays/Easy/268-missing-number/missing-number.py
@@ -1,15 +1,23 @@
 #dsajourney-MV
+#Follow up Question - Gauss’ Sum Formula - TC: O(n); SC: O(1)
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        total = n * (n + 1) // 2
+        actual = sum(n)
+        return toal - sum
+
+#Array based - TC: O(nlogn); SC: O(n)
 class Solution(object):
     def missingNumber(self, nums):
         n = len(nums)
         arr = sorted(nums)
         if arr[-1] != n:
             return n
-        elif arr[0] != 0:
+        if arr[0] != 0:
             return 0
-        else: 
-            for i in range(1, n):
-                if arr[i] - arr[i-1] != 1:
-                    miss = arr[i] - 1
-                    print(miss)
-                    return miss
+        
+        for i in range(1, n):
+            if arr[i] - arr[i-1] != 1:
+                miss = arr[i] - 1
+                return miss
