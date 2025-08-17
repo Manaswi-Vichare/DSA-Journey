b1810ec17e0c4375e66329919e74d40a33f8089d
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-18 03:03:42 +0530

    Time: 12 ms (25.39%) | Memory: 13.6 MB (37.86%) - LeetSync

--- Arrays/Easy/283-move-zeroes/move-zeroes.py
+++ Arrays/Easy/283-move-zeroes/move-zeroes.py
@@ -1,11 +1,19 @@
 #dsajourney-MV
-#Array - TC: O(n); SC: O(1)
+#Follow up Q - Avoiding self swaps - TC: O(n); SC: O(1)
 class Solution(object):
     def moveZeroes(self, nums):
         i = 0
         for j in range(len(nums)):
             if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
+                if i != j:
+                    nums[i], nums[j] = nums[j], nums[i]
                 i += 1
- 
-        
+
+#Array - TC: O(n); SC: O(1)
+# class Solution(object):
+#     def moveZeroes(self, nums):
+#         i = 0
+#         for j in range(len(nums)):
+#             if nums[j] != 0:
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 i += 1
