0b1b472b59e8b11d6e44379d6145352fb9d97dbf
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-18 01:51:34 +0530

    Time: 13 ms (82.94%) | Memory: 26 MB (18.10%) - LeetSync

--- Arrays/Easy/217-contains-duplicate/contains-duplicate.py
+++ Arrays/Easy/217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,22 @@
 #dsajourney-MV
+#HashSet Based - TC: O(n); SC: O(1)
 class Solution(object):
     def containsDuplicate(self, nums):
-        i, count = 0, 0
-        nums.sort()
-        for i in range(0, len(nums)):
-            if nums[i] == nums[i-1]:
-                count += 1
-        if count >= 1 and len(nums) > 1:
-            return True
-        else:
+        s_nums = set(nums)
+        if len(nums) == len(s_nums):
             return False
+        else:
+            return True
+
+#Array Based - TC: O(nlogn); SC: Best- O(1), Worst- O(n)
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         i, count = 0, 0
+#         nums.sort()
+#         for i in range(0, len(nums)):
+#             if nums[i] == nums[i-1]:
+#                 count += 1
+#         if count >= 1 and len(nums) > 1:
+#             return True
+#         else:
+#             return False
