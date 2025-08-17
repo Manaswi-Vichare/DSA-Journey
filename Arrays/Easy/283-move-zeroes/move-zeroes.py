b2cc9568e0e28275deb347e4e2f3c01d9ef4c129
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-18 03:04:03 +0530

    Time: 10 ms (34.48%) | Memory: 13.6 MB (62.17%) - LeetSync

--- Arrays/Easy/283-move-zeroes/move-zeroes.py
+++ Arrays/Easy/283-move-zeroes/move-zeroes.py
@@ -10,10 +10,10 @@ class Solution(object):
                 i += 1
 
 #Array - TC: O(n); SC: O(1)
-# class Solution(object):
-#     def moveZeroes(self, nums):
-#         i = 0
-#         for j in range(len(nums)):
-#             if nums[j] != 0:
-#                 nums[i], nums[j] = nums[j], nums[i]
-#                 i += 1
+class Solution(object):
+    def moveZeroes(self, nums):
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
