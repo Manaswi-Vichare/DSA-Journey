d8ebc7fe61c556bcf0e7f3a8cbd677dcaf9080ca
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-21 21:49:39 +0530

    Time: 69 ms (26.61%) | Memory: 19.7 MB (64.49%) - LeetSync

--- Arrays/Medium/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ Arrays/Medium/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,4 +1,16 @@
 #dsajourney-MV
+#TC: O(n); SC: O(1)
+class Solution(object):
+    def findDuplicates(self, nums):
+        result = []
+        for num in nums:
+            index = abs(num) - 1
+            if nums[index] < 0:
+                result.append(abs(num))
+            else:
+                nums[index] = -nums[index]
+        return result
+
 #TC: O(n); SC: O(n)
 class Solution(object):
     def findDuplicates(self, nums):
@@ -11,3 +23,14 @@ class Solution(object):
             else:
                 seen.add(i) 
         return output
+
+#TC: O(nlogn); SC: O(n)
+class Solution(object):
+    def findDuplicates(self, nums):
+        nums.sort()
+        result=[]
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                result.append(nums[i])
+        return result
+        
