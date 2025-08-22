d129cadaf71dbcf83dcc98a271da3b1b6136e5ea
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-22 22:02:55 +0530

    Time: 39 ms (48.33%) | Memory: 20.8 MB (76.26%) - LeetSync

--- Arrays/Medium/287-find-the-duplicate-number/find-the-duplicate-number.py
+++ Arrays/Medium/287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,11 +1,32 @@
 #dsajourney-MV
-#TC: O(n); SC: O(n)
-class Solution(object):
+#Optimal Solution - Floyd's Tortoise and Hare Cycle Detection - TC: O(n); SC:O(1)
+class Solution:
     def findDuplicate(self, nums):
-        check = [False] * (len(nums)) 
+        slow = nums[0]
+        fast = nums[0]
+
+        # Phase 1: Detect cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Phase 2: Find entry point
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+#TC: O(n); SC: O(n)
+# class Solution(object):
+#     def findDuplicate(self, nums):
+#         check = [False] * (len(nums)) 
     
-        for n in nums:
-            if check[n]:
-                return n
-            check[n] = True
-        return -1
+#         for n in nums:
+#             if check[n]:
+#                 return n
+#             check[n] = True
+#         return -1
