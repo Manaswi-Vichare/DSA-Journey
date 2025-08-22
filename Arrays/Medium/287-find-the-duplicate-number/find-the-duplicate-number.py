c9104f7a75f57010ceaaf07c2b3cb77becc32325
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-22 22:03:08 +0530

    Time: 15 ms (99.39%) | Memory: 21 MB (42.65%) - LeetSync

--- Arrays/Medium/287-find-the-duplicate-number/find-the-duplicate-number.py
+++ Arrays/Medium/287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -21,12 +21,12 @@ class Solution:
         return slow
 
 #TC: O(n); SC: O(n)
-# class Solution(object):
-#     def findDuplicate(self, nums):
-#         check = [False] * (len(nums)) 
+class Solution(object):
+    def findDuplicate(self, nums):
+        check = [False] * (len(nums)) 
     
-#         for n in nums:
-#             if check[n]:
-#                 return n
-#             check[n] = True
-#         return -1
+        for n in nums:
+            if check[n]:
+                return n
+            check[n] = True
+        return -1
