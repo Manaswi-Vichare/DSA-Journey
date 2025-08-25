d22fce73f2a8537399c7b5e02a69b264aff62cae
Manaswi Vichare <83514527+Manaswi-Vichare@users.noreply.github.com>
2025-08-25 21:49:20 +0530

    Time: 10 ms (22.53%) | Memory: 13 MB (73.18%) - LeetSync

---
+++ Arrays/Medium/73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -0,0 +1,23 @@
+#dsajourney-MV
+class Solution(object):
+    def setZeroes(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        rows_to_zero = set()
+        cols_to_zero = set()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows_to_zero.add(i)
+                    cols_to_zero.add(j)
+
+        for i in rows_to_zero:
+            matrix[i] = [0] * n
+            print("2nd", i, j)
+
+        for j in cols_to_zero:
+            for i in range(m):
+                matrix[i][j] = 0
+        return matrix
